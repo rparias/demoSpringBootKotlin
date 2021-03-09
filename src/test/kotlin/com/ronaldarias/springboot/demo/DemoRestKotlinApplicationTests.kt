96446425b589811b339330b29083a01bd7bd8f6f
Ronald Arias <ronald.arias@thoughtworks.com>
2021-03-09 15:07:11 -0500

    first commit

---
+++ src/test/kotlin/com/ronaldarias/springboot/demo/DemoRestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ronaldarias.springboot.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoRestKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
